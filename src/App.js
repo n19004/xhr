ed4aea29a89a9603bc5fc5535b8a21538e4619ad
hikaru <n19004@std.it-college.ac.jp>
2020-01-16 16:03:48 +0900

    授業

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import ValueInput from './ValueInput.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class InchToCm extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      inch: 0,
+      cm: 0
+    }
+  }
+
+  inchChanged (e) {
+    const inchValue = e.value
+    const cmValue = inchValue * 2.54
+    this.setState({
+      inch: inchValue,
+      cm: cmValue
+    })
+  }
+
+  cmChanged (e) {
+    const cmValue = e.value
+    const inchValue = cmValue * 2.54
+    this.setState({
+      inch: inchValue,
+      cm: cmValue
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <ValueInput
+          title='inch'
+          onChange={e => this.inchChanged(e)}
+          value={this.state.inch}
+        />
+        <ValueInput
+          title='cm'
+          onChange={e => this.cmChanged(e)}
+          value={this.state.cm}
+        />
+      </div>
+    )
+  }
 }
-
-export default App;
