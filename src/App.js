20d7951737f64bcbb1bc5beed3ea4b538861e155
hikaru <n19004@std.it-college.ac.jp>
2020-01-21 16:48:09 +0900

    授業

--- src/App.js
+++ src/App.js
@@ -1,26 +1,107 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class FormInput extends Component {
+  constructor (props) {
+    super(props)
+    const v = this.props.value
+    this.state = {
+      value: v,
+      isOK: this.checkValue(v)
+    }
+  }
+
+  checkValue (s) {
+    if (this.props.pattern === null) {
+      return true
+    }
+    return this.props.pattern.test(s)
+  }
+
+  handleChange (e) {
+    const v = e.target.value
+    const filter = this.props.filter
+    let newValue = v
+    if (filter !== null) {
+      newValue = newValue.replace(filter, '')
+    }
+    const newIsOK = this.checkValue(newValue)
+    this.setState({
+      value: newValue,
+      isOK: newIsOK
+    })
+    if (this.props.onChange) {
+      this.props.onChange({
+        target: this,
+        value: newValue,
+        isOK: newIsOK,
+        name: this.props.name
+      })
+    }
+  }
+
+  componentWillReceiveProps (nextProps) {
+    this.setState({
+      value: nextProps.value,
+      isOK: this.checkValue(nextProps.value)
+    })
+  }
+
+  render () {
+    const msg = this.renderStatusMessage()
+    return (
+      <div>
+        <label>
+          {this.props.label}: <br />
+          <input
+            type='text'
+            name={this.props.name}
+            placeholder={this.props.placeholder}
+            value={this.state.value}
+            onChange={e => this.handleChange(e)}
+          />
+          {msg}
+        </label>
+      </div>
+    )
+  }
+
+  renderStatusMessage () {
+    const so = {
+      margin: '8px',
+      padding: '8px',
+      color: 'white'
+    }
+    let msg = null
+    if (this.state.isOK) {
+      // OKのとき
+      so.backgroundColor = 'green'
+      msg = <span style={so}>OK</span>
+    } else {
+      // NGのとき (ただし空白の時は非表示)
+      if (this.state.value !== '') {
+        so.backgroundColor = 'red'
+        msg = <span style={so}>NG</span>
+      }
+    }
+    return msg
+  }
 }
 
-export default App;
+FormInput.propTypes = {
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  filter: PropTypes.object,
+  pattern: PropTypes.object,
+  value: PropTypes.string,
+  placeholder: PropTypes.string,
+  onChange: PropTypes.func
+}
+
+FormInput.defaultProps = {
+  filter: null,
+  pattern: null,
+  value: '',
+  placeholder: '',
+  onChange: null
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import FormInput from './App.js'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class CustomForm extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      email: '',
+      tel: '',
+      allok: false
+    }
+    this.oks = {}
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  handleChange (e) {
+    this.oks[e.name] = e.isOK
+    this.setState({
+      [e.name]: e.value,
+      allok: this.oks['email'] && this.oks['tel']
+    })
+  }
+
+  handleSubmit (e) {
+    window.alert(JSON.stringify(this.state))
+    e.preventDefault()
+  }
+
+  render () {
+    const doChange = e => this.handleChange(e)
+    const doSubmit = e => this.handleSubmit(e)
+    const emailPat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+    const asciiFilter = /[^\u0020-\u007e]+/g
+    return (
+      <form onSubmit={doSubmit}>
+        <FormInput
+          name='email'
+          label='メール'
+          value={this.state.email}
+          filter={asciiFilter}
+          pattern={emailPat}
+          onChange={doChange}
+        />
+        <FormInput
+          name='tel'
+          label='電話番号'
+          value={this.state.tel}
+          filter={/[^0-9-()+]/g}
+          pattern={/^[0-9-()+]+$/}
+          onChange={doChange}
+        />
+        <input type='submit' value='送信' disabled={!this.state.allok} />
+      </form>
+    )
+  }
+}
+
+ReactDOM.render(<CustomForm />, document.getElementById('root'))
