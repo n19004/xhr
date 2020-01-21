c3c4bea037f750cded916a9dd9b3a727d2ed521d
hikaru <n19004@std.it-college.ac.jp>
2020-01-21 15:37:44 +0900

    授業

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class ZipInput extends Component {
+  constructor (props) {
+    super(props)
+    const v = this.props.value ? this.props.value : ''
+    this.state = {
+      value: v,
+      isOK: this.checkValue(v)
+    }
+  }
+
+  checkValue (s) {
+    const zipPattern = /^\d{3}-\d{4}$/
+    return zipPattern.test(s)
+  }
+
+  handleChange (e) {
+    const v = e.target.value
+    const newValue = v.replace(/[^0-9-]+/g, '')
+    const newIsOK = this.checkValue(newValue)
+    this.setState({
+      value: newValue,
+      isOK: newIsOK
+    })
+    if (this.props.onChange) {
+      this.props.onChange({
+        target: this,
+        value: newValue,
+        isOK: newIsOK
+      })
+    }
+  }
+
+  componentWillReceiveProps (nextProps) {
+    this.setState({
+      value: nextProps.value,
+      isOK: this.checkValue(nextProps.value)
+    })
+  }
+
+  render () {
+    const msg = this.renderStatusMessage()
+    return (
+      <div>
+        <label>
+          郵便番号: <br />
+          <input
+            type='text'
+            placeholder='郵便番号を入力'
+            value={this.state.value}
+            onChange={e => this.handleChange(e)}
+          />
+          {msg}
+        </label>
+      </div>
+    )
+  }
+
+  renderStatusMessage () {
+    const so = {
+      margin: '8px',
+      padding: '8px',
+      color: 'white'
+    }
+    let msg = null
+    if (this.state.isOK) {
+      // OKのとき
+      so.backgroundColor = 'green'
+      msg = <span style={so}>OK</span>
+    } else {
+      // NGのとき (ただし空白の時は非表示)
+      if (this.state.value !== '') {
+        so.backgroundColor = 'red'
+        msg = <span style={so}>NG</span>
+      }
+    }
+    return msg
+  }
 }
-
-export default App;
